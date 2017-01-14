875abd2af44d6dffb550fdda2d874102544aed8c
Chinesewangyaxing <2694021518@qq.com>
2017-01-15 00:20:19 +0800

    feat: temp commit

--- src/pages/tabBar.vue
+++ src/pages/tabBar.vue
@@ -1,9 +1,38 @@
 <template>
-    <div>TabBar Component</div>
+    <div class="tab-bar">
+        <ul class="tab-bar-container">
+            <TabBarItem name="首页" :image="require('../assets/home@3x.png')" :focus="true"></TabBarItem>
+            <TabBarItem name="投资" :image="require('../assets/invest@3x.png')"></TabBarItem>
+            <TabBarItem name="发现" :image="require('../assets/find@3x.png')"></TabBarItem>
+            <TabBarItem name="我的" :image="require('../assets/my@3x.png')"></TabBarItem>
+        </ul>
+    </div>
 </template>
 
 <script>
+    import TabBarItem from './tabBarItem';
+
     export default {
+        data() {
+            return {
+            };
+        },
         name: 'tabBar',
+        components: {
+            TabBarItem,
+        },
     };
 </script>
+<style>
+    .tab-bar {
+        width: 100%;
+        height: 49px;
+        background-color: #f1f1f1;
+        position: fixed;
+        bottom: 0;
+    }
+    .tab-bar-container {
+        padding: 6px 0px; 
+    }
+    
+</style>
---
+++ src/pages/tabBarItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="item-container">
+
+        <img :src="image" class="item-img">
+        <p class="item-name ">{{ name }}</p>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        name: 'tabBarItem',
+        props: ['name', 'image', 'focus'],
+    };
+</script>
+
+<style>
+   .item-container {
+       float: left;
+       width: 25%;
+       text-align: center;
+   }
+   .item-name {
+       font-size: 11px;
+   }
+   .active {
+       color: #ff6c00;
+   }
+   .item-img {
+       width: 24px;
+       height: 20px;
+   }
+</style>
