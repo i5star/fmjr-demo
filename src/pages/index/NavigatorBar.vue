b74c7834b6a6df6d67fff8290cfccffd44597f5e
Chinesewangyaxing <2694021518@qq.com>
2017-01-15 11:43:04 +0800

    feat: add navigator and navigatorItem component

--- src/pages/index/NavigatorBar.vue
+++ src/pages/index/NavigatorBar.vue
@@ -1,9 +1,54 @@
 <template>
-    <div class="index-navigator-bar">NavigatorBar</div>
+    <div class="navigatorBar">
+        <div class="navigatorBar-container">
+             <NavigatorBarItem
+                v-for="item of items"
+                :name="item.name"
+                :image="item.image"
+            ></NavigatorBarItem>
+        </div>
+    </div>
 </template>
 
 <script>
+    import NavigatorBarItem from './navigatorBarItem.vue';
+
     export default {
-        name: 'NavigatorBar',
+        data() {
+            return {
+                items: [
+                    {
+                        name: '安全保障',
+                        image: require('../../assets/home_safe@3x.png'),
+                    },
+                    {
+                        name: '新手指引',
+                        image: require('../../assets/home_new@3x.png'),
+                    },
+                    {
+                        name: '帮助中心',
+                        image: require('../../assets/home_help@3x.png'),
+                    },
+                    {
+                        name: '关于分秒',
+                        image: require('../../assets/home_us@3x.png'),
+                    },
+                ],
+            };
+        },
+        name: 'navigatorBar',
+        components: {
+            NavigatorBarItem,
+        },
     };
 </script>
+<style lang="scss">
+    .navigatorBar {
+        width: 100%;
+        overflow: hidden;
+        & > .navigatorBar-container {
+            padding:12px 0px;
+        }
+    }
+    
+</style>
--- src/pages/index/RoadMap.vue
+++ src/pages/index/RoadMap.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="index-roadmap">RoadMap</div>
+    <div class="index-roadmap">
+    </div>
 </template>
 
 <script>
