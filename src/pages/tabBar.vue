87bd45cc024f58bfc12a46e178a41b157eacde07
碎碎酱 <yinxin630@gmail.com>
2017-01-15 09:44:02 +0800

    feat: complete TabBar and TabBarItem component

--- src/pages/tabBar.vue
+++ src/pages/tabBar.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="tab-bar">
         <ul class="tab-bar-container">
-            <TabBarItem name="首页" :image="require('../assets/home@3x.png')" :focus="true"></TabBarItem>
-            <TabBarItem name="投资" :image="require('../assets/invest@3x.png')"></TabBarItem>
-            <TabBarItem name="发现" :image="require('../assets/find@3x.png')"></TabBarItem>
-            <TabBarItem name="我的" :image="require('../assets/my@3x.png')"></TabBarItem>
+            <TabBarItem
+                v-for="(item, index) of items"
+                :name="item.name"
+                :image="item.image"
+                :imageSelected="item.imageSelected"
+                :selected="index === selectedIndex"
+            ></TabBarItem>
         </ul>
     </div>
 </template>
@@ -15,6 +18,29 @@
     export default {
         data() {
             return {
+                items: [
+                    {
+                        name: '首页',
+                        image: require('../assets/home@3x.png'),
+                        imageSelected: require('../assets/home_hover@3x.png'),
+                    },
+                    {
+                        name: '投资',
+                        image: require('../assets/invest@3x.png'),
+                        imageSelected: require('../assets/invest_hover@3x.png'),
+                    },
+                    {
+                        name: '发现',
+                        image: require('../assets/find@3x.png'),
+                        imageSelected: require('../assets/find_hover@3x.png'),
+                    },
+                    {
+                        name: '我的',
+                        image: require('../assets/my@3x.png'),
+                        imageSelected: require('../assets/my_hover@3x.png'),
+                    },
+                ],
+                selectedIndex: 0,
             };
         },
         name: 'tabBar',
